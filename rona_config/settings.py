2ed5f9c8a2d33d070100d0e42c33bb5af5d8ece8
jinho6225 <jinho6225@hotmail.com>
2020-12-01 16:51:23 -0800

    deploying2

--- rona_config/settings.py
+++ rona_config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'hrf15&x1w%(m@o(x0rjw!mg6==y+js0b$zage2b#l%&g63vi!j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
 
 
 # Application definition
